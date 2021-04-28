6af54b6d8242bbb4f7bd5b043cf84fa834567692
Serhii <ramram614@gmail.com>
2021-04-28 14:15:21 +0300

    test 1

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
 
-
 weatherForm.addEventListener('click', (e) => {
     e.preventDefault()
     const location = search.value
